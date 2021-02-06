ebaecb9478b9e884d08645a72d02784695cde695
= <=>
2021-02-06 04:24:53 +0300

    Revert "ilk degisiklik"
    This reverts commit 03be6e9167a2fb3ad4ce9a017ec596bbbdd52929.

--- KriptolojiAffine/main.py
+++ KriptolojiAffine/main.py
@@ -5,7 +5,6 @@ def sifreleme(sifrelenecekMesaj, A, B):
     for i in range(len(sifrelenecekMesaj)):
         harf = (alfabeList.get(sifrelenecekMesaj[i]) * A) + B
         harf = harf % 29
-        #Dictionary valueden key almak
         sifreli_mesaj = sifreli_mesaj + str(list(alfabeList.keys())[list(alfabeList.values()).index(harf)])
     return sifreli_mesaj
 
